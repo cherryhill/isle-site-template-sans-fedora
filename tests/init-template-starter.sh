c094d4438fba2733ff43077034d0fdb768c3173d
flummingbird <wwconlin@gmail.com>
2025-01-27 09:09:38 -0600

    First commit, added isle-site-template.

---
+++ tests/init-template-starter.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+if [ ! -v ISLANDORA_STARTER_REF ] || [ "$ISLANDORA_STARTER_REF" = "" ]; then
+  ISLANDORA_STARTER_REF=heads/main
+fi
+
+if [ ! -v ISLANDORA_STARTER_OWNER ] || [ "$ISLANDORA_STARTER_OWNER" = "" ]; then
+  ISLANDORA_STARTER_OWNER="islandora-devops"
+fi
+
+if [ ! -v ISLANDORA_TAG ] || [ "$ISLANDORA_TAG" = "" ]; then
+  ISLANDORA_TAG=main
+fi
+
+# save the site template default settings.php
+# so we can overwrite it for the starter site
+mv drupal/rootfs/var/www/drupal/assets/patches/default_settings.txt .
+curl -L "https://github.com/${ISLANDORA_STARTER_OWNER}/islandora-starter-site/archive/refs/${ISLANDORA_STARTER_REF}.tar.gz" \
+  | tar --strip-components=1 -C drupal/rootfs/var/www/drupal -xz
+mv default_settings.txt drupal/rootfs/var/www/drupal/assets/patches/default_settings.txt
+
+# copy any drush scripts into the rootfs so we can run them
+cp ./tests/solr.php drupal/rootfs/var/www/drupal/
+
+./generate-certs.sh
+./generate-secrets.sh
+
+docker compose --profile dev build --pull
+docker compose --profile dev up -d
+
+echo "Waiting for installation..."
+docker compose  --profile dev exec drupal-dev timeout 600 bash -c "while ! test -f /installed; do sleep 5; done"
+
+./tests/ping.sh
+
+docker compose --profile dev exec drupal-dev drush scr solr.php
